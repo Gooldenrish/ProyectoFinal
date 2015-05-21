d71d1624befc670e291db024f28d448f13a4cb1a
carlos <carlos@carlos-virtual-machine>
2015-05-21 08:51:51 +0200

    Proyecto Generado

---
+++ ProyectoFinal/src/main/java/daw/es/proyecto/App.java
@@ -0,0 +1,13 @@
+package daw.es.proyecto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
